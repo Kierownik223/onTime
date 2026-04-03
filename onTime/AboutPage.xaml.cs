9c204c4edb4d9e10d30bbd9b8f97e24791e05c13
Kierownik223 <marcel@marmak.net.pl>
2026-04-03 10:37:26 +0200

    Add about page

---
+++ onTime/AboutPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+
+namespace onTime
+{
+    public partial class AboutPage : PhoneApplicationPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicationIcon_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            EmailComposeTask emailTask = new EmailComposeTask();
+            emailTask.To = "mersey@emma.casino";
+            emailTask.Show();
+        }
+    }
+}
